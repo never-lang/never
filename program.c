279c5cf502bf5f47b5815ac113591f7927dee2b5
kuba-- <kuba@sourced.tech>
2018-06-14 12:09:58 +0200

    Add travis script.

--- program.c
+++ program.c
@@ -1,30 +1,24 @@
-#include <malloc.h>
 #include "program.h"
+#include <stdlib.h>
 
-program * program_new(object * params, unsigned int param_count, bytecode * code_arr, unsigned int code_size)
-{
-    program * value = malloc(sizeof(program));
+program* program_new(object* params, unsigned int param_count,
+		     bytecode* code_arr, unsigned int code_size) {
+    program* value = malloc(sizeof(program));
 
     value->params = NULL;
-    value->param_count = 0;    
+    value->param_count = 0;
     value->code_arr = NULL;
     value->code_size = 0;
-    
+
     return value;
 }
 
-void program_delete(program * value)
-{
-    if (value->params != NULL)
-    {
-        object_delete(value->params);
+void program_delete(program* value) {
+    if (value->params != NULL) {
+	object_delete(value->params);
     }
-    if (value->code_arr != NULL)
-    {
-        bytecode_array_delete(value->code_arr);
+    if (value->code_arr != NULL) {
+	bytecode_array_delete(value->code_arr);
     }
     free(value);
 }
-
-
-
