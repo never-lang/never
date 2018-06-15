6bf8ab71886bff02f4eb548eeca00ff6ffbbfd82
smaludzi <smaludzi@github.com>
2018-06-15 19:47:06 +0200

    Reverted incompatibile formatting.

--- back/nev.c
+++ back/nev.c
@@ -19,22 +19,22 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "bytecode.h"
-#include "gencode.h"
-#include "libmath.h"
+#include <errno.h>
 #include "never.h"
-#include "optimize.h"
-#include "parser.h"
-#include "program.h"
 #include "scanner.h"
-#include "tailrec.h"
+#include "parser.h"
 #include "typecheck.h"
-#include "utils.h"
+#include "optimize.h"
+#include "tailrec.h"
+#include "gencode.h"
+#include "bytecode.h"
+#include "program.h"
 #include "vm.h"
+#include "utils.h"
+#include "libmath.h"
 
 #define VM_MEM_SIZE 5000
 #define VM_STACK_SIZE 200
@@ -42,106 +42,121 @@
 #define PARSE_STR 1
 #define PARSE_FILE 2
 
-extern FILE* yyin;
+extern FILE * yyin;
 extern int parse_result;
-extern int yyparse(never**);
 
-int never_func_main_params(never* nev, object** params,
-			   unsigned int* param_count) {
-    object* param = NULL;
-    symtab_entry* entry = NULL;
+int never_func_main_params(never * nev, object ** params, unsigned int * param_count)
+{
+    object * param = NULL;
+    symtab_entry * entry = NULL;
 
     *param_count = 0;
     *params = NULL;
 
     entry = symtab_lookup(nev->stab, "main", SYMTAB_FLAT);
-    if (entry != NULL && entry->type == SYMTAB_FUNC &&
-	entry->func_value != NULL) {
-	func* func_value = entry->func_value;
-	if (func_value->vars != NULL) {
-	    *param_count = func_value->vars->count;
-	    *params = param = malloc(sizeof(object) * (*param_count));
-	    memset(*params, 0, sizeof(object) * (*param_count));
-
-	    var_list_node* node = func_value->vars->tail;
-	    while (node != NULL) {
-		var* value = node->value;
-		if (value->type == VAR_INT) {
-		    param->type = OBJECT_INT;
-		} else if (value->type == VAR_FLOAT) {
-		    param->type = OBJECT_FLOAT;
-		}
-		param++;
-		node = node->next;
-	    }
-	}
+    if (entry != NULL && entry->type == SYMTAB_FUNC && entry->func_value != NULL)
+    {
+        func * func_value = entry->func_value;
+        if (func_value->vars != NULL)
+        {
+            *param_count = func_value->vars->count;
+            *params = param = malloc(sizeof(object) * (*param_count));
+            memset(*params, 0, sizeof(object) * (*param_count));
+            
+            var_list_node * node = func_value->vars->tail;
+            while (node != NULL)
+            {
+                var * value = node->value;
+                if (value->type == VAR_INT)
+                {
+                    param->type = OBJECT_INT; 
+                }
+                else if (value->type == VAR_FLOAT)
+                {
+                    param->type = OBJECT_FLOAT;
+                }
+                param++;
+                node = node->next;
+            }
+        }
     }
-
+    
     return 0;
 }
 
-int parse_prog(program* prog) {
+int parse_prog(program * prog)
+{
     int ret = 0;
-    never* nev = NULL;
+    never * nev = NULL;
 
     parse_result = 0;
     yyparse(&nev);
-    if ((ret = parse_result) == 0) {
-	libmath_add_funcs(nev->funcs);
+    if ((ret = parse_result) == 0)
+    {
+        libmath_add_funcs(nev->funcs);
 
-	ret = never_sem_check(nev);
-	if (ret == 0) {
-	    ret = never_optimize(nev);
-	    if (ret == 0) {
-		ret = never_gencode(nev);
-		never_tailrec(nev);
-		if (ret == 0) {
-		    bytecode_list* code;
+        ret = never_sem_check(nev);
+        if (ret == 0)
+        {
+            ret = never_optimize(nev);
+            if (ret == 0)
+            {
+                ret = never_gencode(nev);
+                never_tailrec(nev);
+                if (ret == 0)
+                {
+                    bytecode_list * code;
 
-		    code = bytecode_new();
+                    code = bytecode_new();
 
-		    never_emit(nev, code);
-		    bytecode_func_addr(code);
+                    never_emit(nev, code);
+                    bytecode_func_addr(code);
 
-		    /* print_functions(nev); */
-		    /* bytecode_print(code); */
+                    /* print_functions(nev); */
+                    /* bytecode_print(code); */
 
-		    never_func_main_params(nev, &prog->params,
-					   &prog->param_count);
-		    bytecode_to_array(code, &prog->code_arr, &prog->code_size);
+                    never_func_main_params(nev, &prog->params, &prog->param_count);
+                    bytecode_to_array(code, &prog->code_arr, &prog->code_size);
 
-		    bytecode_delete(code);
-		}
-	    }
-	}
+                    bytecode_delete(code);
+                }
+            }
+        }
     }
 
-    if (nev != NULL) {
-	never_delete(nev);
+    if (nev != NULL)
+    {
+        never_delete(nev);
     }
-
+    
     return ret;
 }
 
-int parse(const char* input, program* prog, int type) {
+int parse(const char * input, program * prog, int type)
+{
     int ret;
 
-    if (type == PARSE_STR) {
-	scan_string(input);
-    } else if (type == PARSE_FILE) {
-	set_utils_file_name(input);
-
-	yyin = fopen(input, "r");
-	if (yyin == NULL) {
-	    fprintf(stderr, "cannot open %s. %s\n", input, strerror(errno));
-	    exit(1);
-	}
+    if (type == PARSE_STR)
+    {
+        scan_string(input);
+    }
+    else if (type == PARSE_FILE)
+    {
+        set_utils_file_name(input);
+    
+        yyin = fopen(input, "r");
+        if (yyin == NULL)
+        {
+            fprintf(stderr, "cannot open %s. %s\n", input, strerror(errno));
+            exit(1);
+        }
     }
 
     ret = parse_prog(prog);
 
-    if (type == PARSE_FILE) {
-	fclose(yyin);
+    if (type == PARSE_FILE)
+    {
+        fclose(yyin);
     }
 
     yylex_destroy();
@@ -149,20 +164,24 @@ int parse(const char* input, program* prog, int type) {
     return ret;
 }
 
-int parse_str(const char* str, program* prog) {
+int parse_str(const char * str, program * prog)
+{
     return parse(str, prog, PARSE_STR);
 }
 
-int parse_file(const char* file_name, program* prog) {
+int parse_file(const char * file_name, program * prog)
+{
     return parse(file_name, prog, PARSE_FILE);
 }
 
-int execute(program* prog, object* result) {
+int execute(program * prog, object * result)
+{
     int ret = 0;
-    vm* machine = NULL;
+    vm * machine = NULL;
 
-    if (prog->code_arr == NULL) {
-	return 0;
+    if (prog->code_arr == NULL)
+    {
+        return 0;
     }
 
     /* bytecode_array_print(code_arr, code_size); */
@@ -176,37 +195,44 @@ int execute(program* prog, object* result) {
     return ret;
 }
 
-int argc_to_program(program* prog, unsigned int argc, char* argv[]) {
+int argc_to_program(program * prog, unsigned int argc, char * argv[])
+{
     unsigned int i;
-
-    if (prog->param_count > argc) {
-	fprintf(stderr, "too few parameters, expected %d got %d\n",
-		prog->param_count, argc);
-	return 1;
+   
+    if (prog->param_count > argc)
+    {
+        fprintf(stderr, "too few parameters, expected %d got %d\n", prog->param_count, argc);
+        return 1;
     }
-
-    for (i = 0; i < prog->param_count; i++) {
-	if (prog->params[i].type == OBJECT_INT) {
-	    prog->params[i].int_value = atoi(argv[i]);
-	} else if (prog->params[i].type == OBJECT_FLOAT) {
-	    prog->params[i].float_value = atof(argv[i]);
-	}
+    
+    for (i = 0; i < prog->param_count; i++)
+    {
+        if (prog->params[i].type == OBJECT_INT)
+        {
+            prog->params[i].int_value = atoi(argv[i]);
+        }
+        else if (prog->params[i].type == OBJECT_FLOAT)
+        {
+            prog->params[i].float_value = atof(argv[i]);
+        }
     }
-
+    
     return 0;
 }
 
-int parse_and_exec(const char* input, int argc, char* argv[], object* result,
-		   int type) {
+int parse_and_exec(const char * input, int argc, char * argv[], object * result, int type)
+{
     int ret = 0;
-    program* prog = program_new();
+    program * prog = program_new(); 
 
     ret = parse(input, prog, type);
-    if (ret == 0) {
-	ret = argc_to_program(prog, argc, argv);
-	if (ret == 0) {
-	    ret = execute(prog, result);
-	}
+    if (ret == 0)
+    {
+        ret = argc_to_program(prog, argc, argv);
+        if (ret == 0)
+        {
+            ret = execute(prog, result);
+        }
     }
 
     program_delete(prog);
@@ -214,12 +240,13 @@ int parse_and_exec(const char* input, int argc, char* argv[], object* result,
     return ret;
 }
 
-int parse_file_and_exec(const char* file_name, int argc, char* argv[],
-			object* result) {
+int parse_file_and_exec(const char * file_name, int argc, char * argv[], object * result)
+{
     return parse_and_exec(file_name, argc, argv, result, PARSE_FILE);
 }
 
-int parse_str_and_exec(const char* src, unsigned int argc, char* argv[],
-		       object* result) {
+int parse_str_and_exec(const char * src, unsigned int argc, char * argv[], object * result)
+{
     return parse_and_exec(src, argc, argv, result, PARSE_STR);
 }
+
--- main.c
+++ main.c
@@ -21,40 +21,53 @@
  */
 #include <stdio.h>
 #include <string.h>
+#include "nev.h"
 
-#include <nev.h>
+const char * usage = "usage: %s <file name | -e \"one line of program\">\n";
 
-const char* usage = "usage: %s <file name | -e \"one line of program\">\n";
-
-static void print_result(object* result) {
-    if (result->type == OBJECT_INT) {
-	printf("result is %d\n", result->int_value);
-    } else if (result->type == OBJECT_FLOAT) {
-	printf("result is %.2f\n", result->float_value);
-    } else {
-	printf("unknown result type\n");
+static void print_result(object * result)
+{
+    if (result->type == OBJECT_INT)
+    {
+         printf("result is %d\n", result->int_value);
+    }
+    else if (result->type == OBJECT_FLOAT)
+    {
+        printf("result is %.2f\n", result->float_value);
+    }
+    else
+    {
+        printf("unknown result type\n");
     }
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char * argv[])
+{
     int ret = -1;
-    object result = {0};
+    object result = { 0 };
 
-    if (argc < 2) {
-	printf("%s: no input files\n", argv[0]);
-	printf(usage, argv[0]);
-	return 0;
+    if (argc < 2)
+    {
+        printf("%s: no input files\n", argv[0]);
+        printf(usage, argv[0]);
+        return 0;
     }
 
-    if (strncmp("-e", argv[1], 2) == 0) {
-	ret = parse_str_and_exec(argv[2], argc - 3, argv + 3, &result);
-    } else {
-	ret = parse_file_and_exec(argv[1], argc - 2, argv + 2, &result);
+    if (strncmp("-e", argv[1], 2) == 0)
+    {
+        ret = parse_str_and_exec(argv[2], argc - 3, argv + 3, &result);
+    }
+    else
+    {
+        ret = parse_file_and_exec(argv[1], argc - 2, argv + 2, &result);
     }
 
-    if (ret == 0) {
-	print_result(&result);
+    if (ret == 0)
+    {
+        print_result(&result);
     }
 
     return ret;
 }
+
+
